dfc327207a0542e438d5f9cda7fec0aa61e542be
Jdubbs702 <702leopold@gmail.com>
2023-05-19 14:22:32 +0300

    updates

--- scraper.py
+++ scraper.py
@@ -5,8 +5,10 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+import os
 import time
 import pandas as pd
+import requests
 
 # LinkedIn's search is notorious for a few things: displaying irrelevant content, 
 # search limits, and protections against web scraping
@@ -21,35 +23,21 @@ import pandas as pd
 def main():
     start_time = time.time()
     # Define your keywords
-    job_name = "Data Analyst"
-    country_name = "United States"
+    fullStackKey = "Full Stack Developer"
+    frontEndKey = "Front End Developer"
+    backEndKey = "Back End Developer"
+    country_name = "Israel"
+    # country_name = "United States"
 
-    job_url =""
-    for item in job_name.split(" "):
-        if item != job_name.split(" ")[-1]:
-            job_url = job_url + item + "%20"
-        else:
-            job_url = job_url + item
-
-    country_url =""
-    for item in country_name.split(" "):
-        if item != country_name.split(" ")[-1]:
-            country_url = country_url + item + "%20"
-        else:
-            country_url = country_url + item
+    # split the names into words, and join them together w url encoding for the space character
+    job_url = "%20".join(fullStackKey.split(" "))
+    country_url = "%20".join(country_name.split(" "))
 
     # Define url with desired keywords
-    url = "https://www.linkedin.com/jobs/search?keywords={0}&location={1}&geoId=103644278&trk=public_jobs_jobs-search-bar_search-submit&position=1&pageNum=0".format(job_url,country_url)
-
-    # Create a webdriver instance
-    s = Service("C:/Users/Pro/Drivers/chromedriver/chromedriver.exe")
-    driver = webdriver.Chrome(service=s)
-
-    # Opening the url we have just defined in our browser
-    driver.get(url)
-    wait = WebDriverWait(driver, 10)
-    wait.until(EC.presence_of_element_located((By.TAG_NAME, 'body')))
-    wait.until(EC.presence_of_element_located((By.TAG_NAME, 'html')))
+    # url = "https://www.linkedin.com/jobs/search?keywords={0}&location={1}&geoId=103644278&trk=public_jobs_jobs-search-bar_search-submit&position=1&pageNum=0".format(job_url,country_url)
+    url = "https://www.linkedin.com/jobs/search?keywords={0}&location={1}&geoId=101620260&trk=public_jobs_jobs-search-bar_search-submit&position=1&pageNum=0".format(job_url,country_url)
+    # Create a webdriver instance and get page (function defined below)
+    driver = start_driver_get_url(False, url)
     # Find how many jobs there are
     jobs_num = driver.find_element(By.XPATH,'//*[@id="main-content"]/div/h1/span[1]').get_attribute("innerText")
     # remove the + from the string
@@ -74,9 +62,8 @@ def main():
 
     # assuming that after scrolling down half as many times as there are listings, 
     # most or all of the remaining listings will have loaded and will be displayed.
-    i = 2
-    # while i <= int(jobs_num/2)+1:
-    while i <= 2:
+    i = 0
+    while i <= int(jobs_num/2)+1:
         # Keep scrolling down to the end of the view.
         driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
 
@@ -105,52 +92,51 @@ def main():
 
     driver.quit()
 
+    # Define paths to all needed elements
+    paths_dict = {
+        'show_more_path': '/html/body/main/section/div/div/section/div/div/section/button',
+        'seniority_path': '/html/body/main/section/div/div/section/div/ul/li/span',
+        'job_title_path': '/html/body/main/section/div/section[2]/div/div/div/h1',
+        'company_name_path': '/html/body/main/section/div/section[2]/div/div/div/h4/div/span/a',
+        'location_path': '/html/body/main/section/div/section[2]/div/div/div/h4/div/span[2]',
+        'date_path': '/html/body/main/section/div/section[2]/div/div/div/h4/div[2]/span',
+        'jd_parent_path': '/html/body/main/section/div/div/section/div/div/section'
+    }
+    levels = ["Mid-Senior level", "Senior level", "Director", "Executive", "Senior developer", "Tech lead" , "Chief technology officer"]
+
     # Loop over jobs and extract data
     for job_link in job_link_set:
-        try:
-            driver = webdriver.Chrome()
-            # Navigate to job_link
-            driver.get(job_link)
-            wait = WebDriverWait(driver, 10)
-            wait.until(EC.presence_of_element_located((By.TAG_NAME, 'body')))
-            wait.until(EC.presence_of_element_located((By.TAG_NAME, 'html')))
-            # time.sleep(3)
-        except Exception as e:
-            print(f"An error occurred: {str(e)}")
+        retry_count = 0
+        max_retries = 2
+        while retry_count < max_retries:
+            try:
+                driver = start_driver_get_url(False, job_link)
+                # if seniority is too high, go to next job
+                seniority_element = driver.find_element(By.XPATH, paths_dict["seniority_path"])
+                if seniority_element.text in levels:
+                    driver.quit()
+                    break
+            except Exception as e:
+                print(f"An error occurred: {str(e)}")
+                driver.quit()
+                break
 
-        # To reduce # of calls to browser, get all needed elements
-        # in one call
-        paths_dict = {
-            'show_more_path': '/html/body/main/section/div/div/section/div/div/section/button',
-            'seniority_path': '/html/body/main/section/div/div/section/div/ul/li/span',
-            'job_title_path': '/html/body/main/section/div/section[2]/div/div/div/h1',
-            'company_name_path': '/html/body/main/section/div/section[2]/div/div/div/h4/div/span/a',
-            'location_path': '/html/body/main/section/div/section[2]/div/div/div/h4/div/span[2]',
-            'date_path': '/html/body/main/section/div/section[2]/div/div/div/h4/div[2]/span',
-            'jd_parent_path': '/html/body/main/section/div/div/section/div/div/section'
-        }
+            # To reduce # of calls to browser, get all elements from one parent element
+            try:
+                parent = driver.find_element(By.XPATH, "/html/body/main/section/div")
+            except Exception as e:
+                print(f"An error occurred: {str(e)}")
+                driver.quit()
+                break
         
         # collect data from each job
-        #seniority
-        custom_append(driver, find_by_xpath, paths_dict["seniority_path"], "innerText", seniority_list) 
-        #job_title
-        custom_append(driver, find_by_xpath, paths_dict["job_title_path"], "innerText", job_title_list)  
-        #company_name
-        custom_append(driver, find_by_xpath, paths_dict["company_name_path"], "innerText", company_name_list)
-        #location
-        custom_append(driver, find_by_xpath, paths_dict["location_path"], "innerText", location_list)
-        #date
-        custom_append(driver, find_by_xpath, paths_dict["date_path"], "innerText", date_list) 
-        #job_link
-        job_link_list.append(job_link)
-
         #job description
         try:
             # click on show more
-            show_more_element =  driver.find_element(By.XPATH, paths_dict["show_more_path"])
+            show_more_element =  parent.find_element(By.XPATH, paths_dict["show_more_path"])
             show_more_element.click()
             # get job description element
-            parent_element = driver.find_element(By.XPATH, paths_dict["jd_parent_path"])
+            jd_parent_element = parent.find_element(By.XPATH, paths_dict["jd_parent_path"])
             # get all text from all children elements inside job description
             text_list = driver.execute_script("""
                 const parent = arguments[0];
@@ -166,16 +152,51 @@ def main():
                     }
                 }
                 return text_list;
-            """, parent_element)
+            """, jd_parent_element)
             # join together all text but separate with a new line between elements
-            jd_list.append('\n'.join(text_list))
+            jd_text = '\n'.join(text_list)
+#########################################################  THIS SECTION CAN BE COMMENTED OUT
+            # analyse texts and look for "junior" or "entry-level" - continue if false
+            titleText = parent.find_element(By.XPATH, paths_dict["job_title_path"]).get_attribute("innerText").lower()
+            seniorityText = seniority_element.text.lower()
+
+            fullString = jd_text.lower() + titleText + seniorityText
+            if not ("junior" in fullString or "entry level" in fullString):
+                driver.quit()
+                continue
+            print(titleText, seniorityText, jd_text.lower())
+            input("paused")
+#######################################################################################################################
+            # If applicable, only extract the text after "Requirements"
+            if "Requirements" in jd_text:
+                following_text = jd_text[jd_text.index("Requirements") + len("Requirements"):].strip()
+                jd_list.append(following_text)
+            else:
+                jd_list.append('\n'.join(text_list))
         except Exception as e:
             print(f"An error occurred: {str(e)}")
             jd_list.append(None)
+            driver.quit()
+            continue
+        #seniority
+        custom_append(parent, find_by_xpath, paths_dict["seniority_path"], "innerText", seniority_list)
+        #job_title
+        custom_append(parent, find_by_xpath, paths_dict["job_title_path"], "innerText", job_title_list)  
+        #company_name
+        custom_append(parent, find_by_xpath, paths_dict["company_name_path"], "innerText", company_name_list)
+        #location
+        custom_append(parent, find_by_xpath, paths_dict["location_path"], "innerText", location_list)
+        #date
+        custom_append(parent, find_by_xpath, paths_dict["date_path"], "innerText", date_list) 
+        #job_link
+        job_link_list.append(job_link)
+
+        
+
         # close the current window
         driver.quit()
 
-    print(company_name_list, date_list, seniority_list)
+    # print(company_name_list, date_list, seniority_list)
     # Create a Pandas dataFrame for the data
     job_data = pd.DataFrame({
         'Date': date_list,
@@ -184,14 +205,60 @@ def main():
         'Location': location_list,
         'Description': jd_list,
         'Level': seniority_list,
-        'Link': job_link
+        'Link': job_link_list
     })
-    # Create your csv buddy
-    job_data.to_csv('jobs.csv')
+
+    # Shorten URLs in 'Link' column
+    job_data['Link'] = job_data['Link'].apply(shorten_url)
+
+    # Create hyperlinks in 'Link' column
+    job_data['Link'] = job_data['Link'].apply(lambda x: f'=HYPERLINK("{x}", "{x}")')
+
+    # Reset the index
+    job_data.reset_index(drop=True, inplace=True)
+
+    # Write the data to CSV
+    csv = "jobs.csv"
+    csv_exists = os.path.isfile(csv)
+    # Read the existing CSV file (if it exists) to retrieve the maximum index value
+    if csv_exists:
+        existing_data = pd.read_csv(csv)
+        max_index = existing_data.index.max()
+    else:
+        max_index = -1  # If the file doesn't exist, set the max_index to -1
+
+    # Reset the index starting from the maximum index value + 1
+    job_data.reset_index(drop=True, inplace=True)
+    job_data.index += max_index + 1
+
+    mode = 'a' if csv_exists else 'w'
+    header = not csv_exists  # Include header row only if file doesn't exist
+    job_data.to_csv(csv, mode=mode, header=header, encoding='utf-8-sig', escapechar='\\')
 
     end_time = time.time()
     print(f"Program took {end_time - start_time:.2f} seconds to run.")
+
     
+# Define function to shorten URL using TinyURL API
+def shorten_url(url):
+    api_url = "https://tinyurl.com/api-create.php?url="
+    response = requests.get(api_url + url)
+    return response.text
+
+def start_driver_get_url(headlessMode, url):
+    s = Service("C:/Users/Pro/Drivers/chromedriver/chromedriver.exe")
+    options = Options()
+    if headlessMode: 
+        options.headless = True
+        driver = webdriver.Chrome(service=s, options=options)
+    else:
+        driver = webdriver.Chrome(service=s)
+    # Navigate to job_link
+    driver.get(url)
+    wait = WebDriverWait(driver, 10)
+    wait.until(EC.presence_of_element_located((By.TAG_NAME, 'body')))
+    wait.until(EC.presence_of_element_located((By.TAG_NAME, 'html')))
+    return driver
 
 
 def find_by_selector(selector_type):
@@ -201,9 +268,9 @@ find_by_class_name = find_by_selector(By.CLASS_NAME)
 find_by_css_selector = find_by_selector(By.CSS_SELECTOR)
 find_by_xpath = find_by_selector(By.XPATH)
 
-def custom_append(job, find_func, selector_value, attribute, target_list):
+def custom_append(element, find_func, selector_value, attribute, target_list):
     try:
-        data = find_func(job, selector_value, attribute)
+        data = find_func(element, selector_value, attribute)
         if data:
             target_list.append(data)
         else:
@@ -214,19 +281,3 @@ def custom_append(job, find_func, selector_value, attribute, target_list):
 
 
 main()
-    # # Login and enter the email address and password in the input fields
-    # driver.get("https://www.linkedin.com/login")
-    # email_field = driver.find_element(By.ID,"username")
-    # email_field.send_keys("jeremyleopold.hw@gmail.com")
-
-    # password_field = driver.find_element(By.ID,"password")
-    # password_field.send_keys("LeoChief10")
-    # # Submit the form by pressing the "Enter" key
-    # password_field.send_keys(Keys.RETURN)
-
-    # # Wait for the login process to complete and the page to load
-    # time.sleep(3)
-    # input("If security check, satisfy and then press enter. Otherwise, just press enter")
-
-    # # Verify that the login was successful by checking the page title
-    # assert "LinkedIn" in driver.title
